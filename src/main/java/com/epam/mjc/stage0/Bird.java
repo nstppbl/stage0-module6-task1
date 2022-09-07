9b46eca41a2e9096dab0f2048ea9eb8206259751
nstppbl <1nstppbl@gmail.com>
2022-09-07 11:17:08 +0500

    initial commit

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -5,4 +5,8 @@ public class Bird extends Animal {
     public Bird(){
         super("blue", 2, false);
     }
+
+    public String getDescription() {
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -6,9 +6,6 @@ public class Dog extends Animal {
         super("Brown", 4, true);
     }
 
-    public String getDescription() {
-            return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
-        }
 }
 
 
